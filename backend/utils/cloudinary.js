8ab73af2d227624f313a011a769297dd110aa18c
DIBESH KUMAR PARIDA <dibeshkumarparida2610@gmail.com>
2026-03-07 23:47:51 +0530

    Implement doctor controller

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -3,8 +3,41 @@ import fs from 'fs';
 
 
 //configure cloudinary
-cloudinary.config({                     
+cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+//to upload files to cloudinary
+export async function uploadToCloudinary(filePath, folder = "Doctor") {
+    try {
+        const result = await cloudinary.uploader.upload(filePath, {
+            folder,
+            resource_type: "image",
+        });
+        //remove the local file after upload
+        fs.unlinkSync(filePath);
+        return result;
+    }
+
+    catch (err) {
+        console.error('Error uploading to Cloudinary:', err);
+        throw err;
+    }
+}
+
+//to delete an image that is present in cloudinary if user removes from ui
+export async function deleteFromCloudinary(publicId) {
+    try {
+        if (!publicId) return; // If no publicId is provided, skip deletion
+        await cloudinary.uploader.destroy(publicId);
+    }
+
+    catch (err) {
+        console.error('Error deleting from Cloudinary:', err);
+        throw err;
+    }
+}
+export default cloudinary;
+
