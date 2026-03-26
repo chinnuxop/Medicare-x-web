3949d49ac0b7c778852233dc780e2101818354b7
DIBESH KUMAR PARIDA <dibeshkumarparida2610@gmail.com>
2026-03-26 15:47:10 +0530

    feat: add certification section to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import Banner from '../components/Banner';
+import Certification from '../components/Certification';
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <div>
         <Navbar />
         <Banner />
+        <Certification />
     </div>
   )
 }
