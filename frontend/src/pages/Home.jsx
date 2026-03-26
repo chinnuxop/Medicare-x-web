8dc6c1183a57c6bc7d8fde6685abf0e15aa8d0ae
DIBESH KUMAR PARIDA <dibeshkumarparida2610@gmail.com>
2026-03-26 23:04:33 +0530

    feat: add medical team to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import Banner from '../components/Banner';
 import Certification from '../components/Certification';
+import HomeDoctors from '../components/HomeDoctors';
 
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
         <Navbar />
         <Banner />
         <Certification />
+        <HomeDoctors/>
     </div>
   )
 }
