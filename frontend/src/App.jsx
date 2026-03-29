91b99d98cd871907859ae8898608dc85d16ae6a4
DIBESH KUMAR PARIDA <dibeshkumarparida2610@gmail.com>
2026-03-29 15:07:24 +0530

    feat: navigate to doctor details page on Book Now click

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home";
 import Doctors from "./pages/Doctors";
+import DoctorDetail from "./pages/DoctorDetail";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/doctors" element={<Doctors />} />
+      <Route path="/doctors/:id" element={<DoctorDetail />} />
     </Routes>
   )
 }
